0f16bc8092cf96cd9b5e9c1738c14f67e4e33870
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2022-01-07 15:07:08 -0800

    added airnow_PM2.5 elevations

---
+++ AirNow/addMissingElevation.R
@@ -0,0 +1,64 @@
+# Use a web service to add missing elevation data
+
+library(MazamaLocationUtils)
+
+collectionName <- "airnow_PM2.5_sites"
+
+# ----- Load and Review --------------------------------------------------------
+
+setLocationDataDir("./AirNow")
+
+locationTbl <- table_load(collectionName)
+
+missingElevation_mask <- is.na(locationTbl$elevation)
+
+locationTbl[missingElevation_mask,] %>%
+  MazamaLocationUtils::table_leaflet(extraVars = "elevation")
+
+# ----- Add elevations ---------------------------------------------------------
+
+# NOTE:  This is a manual operation so print out progress
+for ( i in which(missingElevation_mask) ) {
+
+  if ( (i %% 10) == 0 )
+    message(sprintf("Working on %d ...", i))
+
+  result <- try({
+
+    locationTbl$elevation[i] <-
+      location_getSingleElevation_USGS(
+        longitude = locationTbl$longitude[i],
+        latitude = locationTbl$latitude[i]
+      )
+
+  }, silent = FALSE)
+
+  if ( "try-error" %in% class(result) ) {
+    next
+  }
+
+}
+
+# ----- Review -----------------------------------------------------------------
+
+locationTbl[missingElevation_mask,] %>%
+  MazamaLocationUtils::table_leaflet(extraVars = "elevation")
+
+
+# ----- Save the table ---------------------------------------------------------
+
+table_save(
+  locationTbl,
+  collectionName = collectionName,
+  backup = FALSE,
+  outputType = "csv"
+)
+
+table_save(
+  locationTbl,
+  collectionName = collectionName,
+  backup = FALSE,
+  outputType = "rda"
+)
+
+
