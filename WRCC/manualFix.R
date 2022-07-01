7b4a4aa5d6f1ccca1d5fe74ae6b3f6d82b224f69
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2022-07-01 10:31:07 -0700

    added "MetOne" locationName

---
+++ WRCC/manualFix.R
@@ -0,0 +1,61 @@
+# NOTE:  Modify this script to fix one field at a time
+
+# Use a web service to add missing elevation data
+
+library(MazamaLocationUtils)
+
+collectionDir <- "WRCC"
+collectionName <- "wrcc_PM2.5_sites_1000"
+collectionFile <- paste0(collectionName, ".rda")
+
+# ----- Load and Review --------------------------------------------------------
+
+download.file(
+  file.path("http://data-monitoring_v2-c1.airfire.org/monitoring-v2/known-locations", collectionFile),
+  destfile = file.path(".", collectionDir, collectionFile)
+)
+
+setLocationDataDir(file.path(".", collectionDir))
+
+locationTbl <- table_load(collectionName)
+dim(locationTbl)
+
+
+# ----- Problem to fix ---------------------------------------------------------
+
+locationTbl %>%
+  MazamaLocationUtils::table_leaflet(
+    extraVars = c("locationName", "fullAQSID"),
+    jitter = 0
+  )
+
+# "Rocky Mtn Fire Cache" west of Denver, CO
+locationIDs <- c("6594e05e882c2c1e")
+locationTbl[locationTbl$locationID %in% locationIDs, "locationName"] <- "Rocky Mtn Fire Cache"
+
+# "Met One" headquarters in Grants Pass, OR
+locationIDs <- c("e2b1634a0cf7d694")
+locationTbl[locationTbl$locationID %in% locationIDs, "locationName"] <- "Met One"
+
+
+# Review
+locationTbl %>% MazamaLocationUtils::table_leaflet(extraVars = "locationName")
+
+
+# ----- Save the table ---------------------------------------------------------
+
+table_save(
+  locationTbl,
+  collectionName = collectionName,
+  backup = FALSE,
+  outputType = "csv"
+)
+
+table_save(
+  locationTbl,
+  collectionName = collectionName,
+  backup = FALSE,
+  outputType = "rda"
+)
+
+
